dba9b0dfe59eb4b680d00a4d2862182687c53730
dhemery <dhemery@8dd66848-6e4f-0410-86f9-53bc9dbbc27c>
2008-06-19 12:49:05 +0000

    Ported from SWT to Swing. No layout yet.  That's next.

--- com.dhemery.gibberizer/src/com/dhemery/gibberizer/listeners/SpinnerListener.java
+++ com.dhemery.gibberizer/src/com/dhemery/gibberizer/listeners/SpinnerListener.java
@@ -1,12 +1,13 @@
 package com.dhemery.gibberizer.listeners;
 
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
-import org.eclipse.swt.widgets.Spinner;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import com.dhemery.gibberizer.core.Gibberizer;
 
-public abstract class SpinnerListener implements SelectionListener {
+public abstract class SpinnerListener implements ChangeListener {
 
 	private final Gibberizer gibberizer;
 
@@ -17,14 +18,10 @@ public abstract class SpinnerListener implements SelectionListener {
 	}
 
 	@Override
-	public void widgetDefaultSelected(SelectionEvent e) {
-		widgetSelected(e);
-	}
-
-	@Override
-	public void widgetSelected(SelectionEvent e) {
-		Spinner spinner = (Spinner)e.widget;
-		execute(spinner.getSelection());
+	public void stateChanged(ChangeEvent e) {
+		JSpinner spinner = (JSpinner)e.getSource();
+		SpinnerNumberModel model = (SpinnerNumberModel)spinner.getModel();
+		execute(model.getNumber().intValue());
 	}
 
 	public Gibberizer getGibberizer() {
