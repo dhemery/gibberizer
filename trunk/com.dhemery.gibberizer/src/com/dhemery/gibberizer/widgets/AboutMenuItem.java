607e19a2ad7c4e77d2e5969f4390a2d63b62a765
dhemery <dhemery@8dd66848-6e4f-0410-86f9-53bc9dbbc27c>
2008-06-21 03:39:00 +0000

    Changed the About dialog to plain text.  Yep, it's ugly, but it gives the user something to copy and past into a browser, and it will suffice until I learn how to do something more useful.

--- trunk/com.dhemery.gibberizer/src/com/dhemery/gibberizer/widgets/AboutMenuItem.java
+++ trunk/com.dhemery.gibberizer/src/com/dhemery/gibberizer/widgets/AboutMenuItem.java
@@ -3,46 +3,32 @@
  */
 package com.dhemery.gibberizer.widgets;
 
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
 
 import com.dhemery.gibberizer.ui.GibberizerWindow;
 
 public class AboutMenuItem extends GibberizerMenuItem {
 	private static final long serialVersionUID = 1L;
 	private static final String aboutMessage = 
-		"<html>" +
-			"Learn more about " +
-			GibberizerWindow.APPLICATION_BASE_NAME +
-			" at:" +
-			"<ul>" +
-				"<li>" +
-					"The " +
-					"<a href='http://code.google.com/p/gibberizer/'>" +
-						GibberizerWindow.APPLICATION_BASE_NAME + " Web Site" +
-					"</a>" +
-				"</li>" +
-				"<li>" +
-					"The " +
-					"<a href='http://code.google.com/p/gibberizer/wiki/UserGuide'>" +
-						GibberizerWindow.APPLICATION_BASE_NAME + " User Guide" +
-					"</a>" +
-				"</li>" +
-				"<li>" +
-					"The " +
-					"<a href='http://code.google.com/p/gibberizer/w/list'>" +
-						GibberizerWindow.APPLICATION_BASE_NAME + " Wiki" +
-					"</a>" +
-				"</li>" +
-			"</ul>" +
-		"</html>";
+		"For more information about Gibberizer, " +
+		"visit the Gibberizer Project Page at " +
+		"http://code.google.com/p/gibberizer/";
+	private final JTextArea messageBox;
+
 	public AboutMenuItem(String name, GibberizerWindow window) {
 		super(name, window);
+		messageBox = new JTextArea(aboutMessage, 3, 38);
+		messageBox.setEditable(false);
+		messageBox.setLineWrap(true);
+		messageBox.setWrapStyleWord(true);
 	}
 
 	@Override
 	public void execute(GibberizerWindow window) {
 		JOptionPane.showMessageDialog(window,
-			    aboutMessage,
+				messageBox,
 			    getText(),
 			    JOptionPane.PLAIN_MESSAGE);
 	}
