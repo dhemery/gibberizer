42f8d1764f1e6bcd373e7e0cfa8b1e3514e9cdbb
dhemery <dhemery@8dd66848-6e4f-0410-86f9-53bc9dbbc27c>
2008-06-22 10:17:31 +0000

    Refactored to clarify the Ngram extractor tests.
    Removed Ngram.equals(), which was used only in tests.  Also removed the associated tests.

--- trunk/com.dhemery.gibberizer/unittests/com/dhemery/gibberizer/unittests/NgramBagTests.java
+++ trunk/com.dhemery.gibberizer/unittests/com/dhemery/gibberizer/unittests/NgramBagTests.java
@@ -1,10 +1,7 @@
 package com.dhemery.gibberizer.unittests;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import com.dhemery.gibberizer.core.NgramBag;
--- trunk/com.dhemery.gibberizer/unittests/com/dhemery/gibberizer/unittests/NgramTests.java
+++ trunk/com.dhemery.gibberizer/unittests/com/dhemery/gibberizer/unittests/NgramTests.java
@@ -1,27 +1,18 @@
 package com.dhemery.gibberizer.unittests;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import com.dhemery.gibberizer.core.Ngram;
 
 public class NgramTests {
 
-	private String string;
-	private int n;
-
-	@Before
-	public void setUp() {
-		n = 6;
-		string = "abcdefghij";
-	}
-
 	@Test
 	public void prefixMatchesFirstNMinusOneCharactersOfGivenString() {
+		int n = 6;
+		String string = "abcdefghij";
 		String desiredPrefix = string.substring(0, n - 1);
 		Ngram ngram = new Ngram(string, n);
 		assertEquals(desiredPrefix, ngram.getPrefix());
@@ -29,6 +20,8 @@ public class NgramTests {
 
 	@Test
 	public void suffixMatchesCharactersOneThroughNOfGivenString() {
+		int n = 6;
+		String string = "abcdefghij";
 		String desiredSuffix = string.substring(1, n);
 		Ngram ngram = new Ngram(string, n);
 		assertEquals(desiredSuffix, ngram.getSuffix());
@@ -36,12 +29,16 @@ public class NgramTests {
 
 	@Test
 	public void lastCharacterHasLengthOne() {
+		int n = 6;
+		String string = "abcdefghij";
 		Ngram ngram = new Ngram(string, n);
 		assertEquals(1, ngram.getLastCharacter().length());
 	}
 
 	@Test
 	public void lastCharacterMatchesCharacterAtPositionNMinusOneFromGivenString() {
+		int n = 6;
+		String string = "abcdefghij";
 		Ngram ngram = new Ngram(string, n);
 		String desiredSuffix = string.substring(n - 1, n);
 		assertEquals(desiredSuffix, ngram.getLastCharacter());
@@ -49,60 +46,26 @@ public class NgramTests {
 
 	@Test
 	public void prefixEqualsGivenStringIfGivenStringIsShorterThanN() {
-		n = string.length() + 1;
+		String string = "abcdefghij";
+		int n = string.length() + 1;
 		Ngram ngram = new Ngram(string, n);
 		assertEquals(string, ngram.getPrefix());
 	}
 
 	@Test
 	public void suffixMatchesTailOfGivenStringIfGivenStringIsShorterThanN() {
-		n = string.length() + 1;
+		String string = "abcdefghij";
+		int n = string.length() + 1;
 		Ngram ngram = new Ngram(string, n);
 		assertTrue(string.endsWith(ngram.getSuffix()));
 	}
 
 	@Test
 	public void lastCharacterIsEmptyIfGivenStringIsShorterThanN() {
-		n = string.length() + 1;
+		String string = "abcdefghij";
+		int n = string.length() + 1;
 		Ngram ngram = new Ngram(string, n);
 		assertTrue(ngram.getLastCharacter().length() == 0);
-	}
-
-	@Test
-	public void equalIfSamePrefixAndLastLetter() {
-		Ngram ngram1 = new Ngram(string, n);
-		Ngram ngram2 = new Ngram(string, n);
-		assertTrue(ngram1.equals(ngram2));
-	}
-
-	@Test
-	public void notEqualIfDifferentPrefix() {
-		Ngram ngram1 = new Ngram("abcdefg", 4);
-		Ngram ngram2 = new Ngram("bbcdefg", 4);
-		assertFalse(ngram1.equals(ngram2));
-	}
-
-	@Test
-	public void notEqualIfDifferentLastLetter() {
-		Ngram ngram1 = new Ngram("abcdefg", 4);
-		Ngram ngram2 = new Ngram("abceefg", 4);
-		assertFalse(ngram1.equals(ngram2));
-	}
-
-	@Test
-	public void equalityIsCaseInsensive() {
-		Ngram ngramLower = new Ngram("abcd", 4);
-		Ngram ngramUpper = new Ngram("ABCD", 4);
-		assertTrue(ngramLower.equals(ngramUpper));
-	}
-
-	@Test
-	public void equalStringConversionDoesNotImplyEqualNgrams() {
-		Ngram ngram1 = new Ngram("abcd", 4); // Prefix "abc", Last char "d"
-		Ngram ngram2 = new Ngram("abcd", 5); // Prefix "abcd", Last char ""
-		assertEquals(ngram1.toString(), ngram2.toString()); // To demonstrate
-															// equal toString()
-		assertFalse(ngram1.equals(ngram2));
 	}
 
 	@Test
@@ -122,6 +85,8 @@ public class NgramTests {
 
 	@Test
 	public void toStringEqualsPrefixPlusLastCharacter() {
+		int n = 6;
+		String string = "abcdefghij";
 		Ngram ngram = new Ngram(string, n);
 		assertEquals(ngram.getPrefix() + ngram.getLastCharacter(), ngram
 				.toString());
