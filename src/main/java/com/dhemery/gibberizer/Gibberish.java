1447fbac2acd67c585262f27c2e1acb1cf67ed8d
Dale Emery <dale@dhemery.com>
2017-12-28 19:37:23 -0800

    Minor refactoring

--- src/main/java/com/dhemery/gibberizer/Gibberish.java
+++ src/main/java/com/dhemery/gibberizer/Gibberish.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import java.util.Random;
 import java.util.function.Supplier;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.joining;
@@ -34,13 +35,19 @@ public class Gibberish implements Supplier<String> {
 
     @Override
     public String get() {
-        return positionStream()
-                .limit(size)
-                .mapToObj(this::suffixOf)
-                .collect(joining("", prefixOf(overlap), ""));
+        return gibberishStream()
+                .collect(joining());
     }
 
-    private IntStream positionStream() {
+    private Stream<String> gibberishStream() {
+        return Stream.concat(Stream.of(prefixOf(overlap)), suffixStream()).limit(size);
+    }
+
+    private Stream<String> suffixStream() {
+        return suffixPositionStream().mapToObj(this::suffixOf);
+    }
+
+    private IntStream suffixPositionStream() {
         return IntStream.iterate(overlap, this::hasNext, this::next);
     }
 
