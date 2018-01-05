0b8d74e6261b14e9dc16564a30dfcbd18028abf0
Dale Emery <dale@dhemery.com>
2018-01-04 18:33:41 -0800

    generate() using stream
    Currently uses a stream of lines randomly selected from input
    strings.

--- src/main/java/com/dhemery/gibberizer/application/GibberizerController.java
+++ src/main/java/com/dhemery/gibberizer/application/GibberizerController.java
@@ -2,18 +2,26 @@ package com.dhemery.gibberizer.application;
 
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.StringExpression;
-import javafx.beans.property.ListProperty;
-import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.*;
 import javafx.beans.value.ObservableObjectValue;
 import javafx.beans.value.ObservableStringValue;
 import javafx.collections.FXCollections;
 
 import java.util.List;
+import java.util.Random;
 import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
 public class GibberizerController {
+    private final IntegerProperty similarity = new SimpleIntegerProperty(2);
+    private final IntegerProperty persistence = new SimpleIntegerProperty(3);
+    private final IntegerProperty batchSize = new SimpleIntegerProperty(4);
+    private final SetProperty<String> distinctInputStrings = new SimpleSetProperty<>(FXCollections.emptyObservableSet());
+
     private final ObservableStringValue inputText;
     private final ObservableObjectValue<Function<String, List<String>>> inputSplitter;
     private final StringExpression gibberish;
@@ -22,10 +30,20 @@ public class GibberizerController {
     public GibberizerController(ObservableStringValue inputText, ObservableObjectValue<Function<String, List<String>>> inputSplitter, ObservableStringValue outputSeparator) {
         this.inputText = inputText;
         this.inputSplitter = inputSplitter;
+        // TODO: save batchSize parameter
+        // TODO: save persistence parameter
+        // TODO: take similarity parameter
 
         gibberish = Bindings.createStringBinding(
                 () -> gibberishList.stream().collect(joining((outputSeparator.get()))),
                 gibberishList, outputSeparator);
+
+        // TODO: inputStrings property: (inputText, inputSplitter) -> List<String>
+        // TODO: distinctInputStrings property: (inputStrings) -> Set<String>
+        // TODO: nGrams property: (inputStrings, similarity) -> List<NGram>
+        // TODO: starterSupplier property: (nGramList) -> Supplier<NGram>
+        // TODO: successorOperator property: (nGramList) -> UnaryOperator<NGram>
+        // TODO: gibberizer property: (starterSupplier, successorOperator) -> Supplier<String>
     }
 
     public StringExpression gibberish() {
@@ -34,6 +52,18 @@ public class GibberizerController {
 
     public void generate() {
         List<String> inputStrings = inputSplitter.get().apply(inputText.getValue());
-        gibberishList.set(FXCollections.observableList(inputStrings));
+        Random random = new Random();
+        Supplier<String> generator = () -> inputStrings.get(random.nextInt(inputStrings.size()));
+        // TODO: Use real gibberizer to generate gibberish
+        List<String> gibberishStrings = Stream.generate(generator)
+                .limit(persistence.get() * batchSize.get())
+                // TODO: Apply filter only if inputs are disallowed in output
+                // .filter(s -> !distinctInputStrings.getValue().contains(s))
+                .distinct()
+                .limit(batchSize.get())
+                .map(s -> s + "foo")
+                .collect(toList());
+
+        gibberishList.set(FXCollections.observableList(gibberishStrings));
     }
 }
