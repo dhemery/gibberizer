0b3a5b6cda234306c4073ff99f8a92a01b82342c
Dale Emery <dale@dhemery.com>
2018-01-07 13:14:05 -0800

    Simplify binding chains in controller

--- src/main/java/com/dhemery/gibberizer/application/GibberizerController.java
+++ src/main/java/com/dhemery/gibberizer/application/GibberizerController.java
@@ -3,7 +3,7 @@ package com.dhemery.gibberizer.application;
 import com.dhemery.gibberizer.core.GibberishSupplier;
 import com.dhemery.gibberizer.core.NGram;
 import com.dhemery.gibberizer.core.NGramParser;
-import javafx.beans.binding.ListBinding;
+import javafx.beans.binding.Bindings;
 import javafx.beans.binding.ObjectBinding;
 import javafx.beans.binding.StringBinding;
 import javafx.beans.property.*;
@@ -22,9 +22,6 @@ import java.util.stream.Stream;
 import static java.util.stream.Collectors.*;
 
 public class GibberizerController {
-    private static final Function<String, List<String>> PARSE_INPUT_AS_WORDS = s -> Arrays.asList(s.split("\\s+"));
-    private static final Function<String, List<String>> PARSE_INPUT_AS_LINES = s -> Arrays.asList(s.split("\\s*\\n\\s*"));
-    private static final Function<String, List<String>> PARSE_INPUT_AS_STRING = Arrays::asList;
     private static final Random RANDOM = new Random();
     private final ListProperty<String> gibberishList = new SimpleListProperty<>(FXCollections.observableArrayList());
     private final ObjectProperty<Function<String, List<String>>> inputSplitter = new SimpleObjectProperty<>();
@@ -34,13 +31,7 @@ public class GibberizerController {
     @FXML
     private CheckBox splitInput;
     @FXML
-    private Toggle splitIntoWords;
-    @FXML
-    private Toggle splitIntoLines;
-    @FXML
     private TextArea inputText;
-    @FXML
-    private Button generateButton;
     @FXML
     private Spinner<Integer> batchSize;
     @FXML
@@ -55,28 +46,29 @@ public class GibberizerController {
     private Text outputText;
 
     public void initialize() {
-        Map<Toggle, Function<String, List<String>>> splittersByToggle = new HashMap<>();
-        splittersByToggle.put(splitIntoWords, PARSE_INPUT_AS_WORDS);
-        splittersByToggle.put(splitIntoLines, PARSE_INPUT_AS_LINES);
+        StringBinding selectedInputSplitterPattern = userDataOf(inputSplitterToggles.selectedToggleProperty());
+        ObjectBinding<Function<String, List<String>>> selectedInputSplitter = Bindings.createObjectBinding(
+                () -> t -> List.of(t.split(selectedInputSplitterPattern.get())),
+                selectedInputSplitterPattern
+        );
 
-        inputSplitter.bind(inputSplitterBinding(splitInput.selectedProperty(), inputSplitterToggles.selectedToggleProperty(), splittersByToggle));
-        outputText.textProperty().bind(outputTextBinding(gibberishList, outputFormatToggles.selectedToggleProperty()));
+        inputSplitter.bind(
+                Bindings.when(splitInput.selectedProperty())
+                .then(selectedInputSplitter)
+                .otherwise(new SimpleObjectProperty<>(List::of)));
+
+        StringBinding selectedOutputDelimiter = userDataOf(outputFormatToggles.selectedToggleProperty());
+        outputText.textProperty().bind(Bindings.createStringBinding(
+                () -> gibberishList.stream().collect(joining((selectedOutputDelimiter.get()))),
+                gibberishList, selectedOutputDelimiter
+        ));
     }
 
     public void generate() {
-        List<String> errorLines = new ArrayList<>();
         List<String> inputStrings = new ArrayList<>(inputSplitter.get().apply(inputText.textProperty().get()));
         if (inputStrings.removeIf(s -> s.length() < similarity.getValue())) ;
         if (inputStrings.isEmpty()) {
-            errorLines.add("Gibberizer could not generate gibberish from such short strings.");
-            errorLines.add("Try:");
-            if (similarity.getValue() > 2) errorLines.add("- decreasing the similarity");
-            errorLines.add("- writing more text");
-            if (splitInput.isSelected()) {
-                if (splitIntoWords.isSelected()) errorLines.add("- splitting the text into lines instead of words");
-                errorLines.add("- not splitting the text");
-            }
-            gibberishList.setAll(errorLines.stream().collect(joining("\n")));
+            gibberishList.clear();
             return;
         }
         Set<String> distinctInputStrings = new HashSet<>(inputStrings);
@@ -98,15 +90,7 @@ public class GibberizerController {
                 .limit(batchSize.getValue())
                 .collect(toList());
 
-        if (gibberishStrings.isEmpty()) {
-            gibberishList.setAll("Could not generate gibberish from the given text and settings.");
-            return;
-        }
-
         gibberishList.setAll(gibberishStrings);
-    }
-
-    private void warn(String s) {
     }
 
     private static <T> T selectRandom(List<? extends T> list) {
@@ -114,45 +98,7 @@ public class GibberizerController {
         return list.get(RANDOM.nextInt(list.size()));
     }
 
-    private static ListBinding<String> inputStringsBinding(StringProperty text, ObjectProperty<Function<String, List<String>>> inputSplitter) {
-        return new ListBinding<>() {
-            {
-                super.bind(text, inputSplitter);
-            }
-
-            @Override
-            protected ObservableList<String> computeValue() {
-                return FXCollections.observableList(inputSplitter.get().apply(text.get()));
-            }
-        };
-    }
-
-    private static ObjectBinding<? extends Function<String, List<String>>> inputSplitterBinding(BooleanProperty shouldSplit, ReadOnlyObjectProperty<Toggle> selectedSplitterToggle, Map<Toggle, Function<String, List<String>>> splittersByToggle) {
-        return new ObjectBinding<>() {
-            {
-                super.bind(shouldSplit, selectedSplitterToggle);
-            }
-
-            @Override
-            protected Function<String, List<String>> computeValue() {
-                if (shouldSplit.get()) {
-                    return splittersByToggle.get(selectedSplitterToggle.get());
-                }
-                return PARSE_INPUT_AS_STRING;
-            }
-        };
-    }
-
-    private static StringBinding outputTextBinding(ObservableList<String> gibberishList, ReadOnlyObjectProperty<Toggle> selectedOutputFormatToggle) {
-        return new StringBinding() {
-            {
-                super.bind(gibberishList, selectedOutputFormatToggle);
-            }
-
-            @Override
-            protected String computeValue() {
-                return gibberishList.stream().collect(joining((selectedOutputFormatToggle.get().getUserData().toString())));
-            }
-        };
+    private static StringBinding userDataOf(ReadOnlyObjectProperty<Toggle> toggle) {
+        return Bindings.createStringBinding(() -> toggle.get().getUserData().toString(), toggle);
     }
 }
